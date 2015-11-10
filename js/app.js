931228eb8c6341e1531f72efd04be7e45eecedc1
JamesBrowning1991 <jamesbrowning91@gmail.com>
2015-11-10 17:32:11 +0000

    Attempt to get band gig data from bandsintown
    but not successful

--- js/app.js
+++ js/app.js
@@ -11,33 +11,51 @@ var setupGigsPage = function() {
   $("#logo").animate({marginTop: "-=15%"}, 1000);
   $("#button-ul").animate({marginTop: "+=15%"}, {duration: 1000, queue: false});
   $("#button-ul").fadeOut(1000, function() {
-    $("h1").text("Comming soon..").hide().fadeIn(500);
+    //$("h1").text("Comming soon..").hide().fadeIn(500);
+    gigsInfo()
   })
 };
 
 
+// Function to get gigs info and display CANT GET THIS TO WORK, NEED PROXY http://www.ajax-cross-origin.com/how.html
+var gigsInfo = function() {
+  //var url = "http://api.bandsintown.com/artists/CountlessSkies/events.json?app_id=123"
+
+  $.ajax({
+    url: "http://api.bandsintown.com/artists/CountlessSkies/events.json?app_id=123",
+    crossDomain: true
+  })
+    .done(function( data ) {
+      if ( console && console.log ) {
+        console.log( data );
+      }
+    });
+}
+
+
 // fade in on startup
-mainPageSetup()
+mainPageSetup();
 
-// // Wait for device API libraries to load
-// function onLoad() {
-document.addEventListener("deviceready", onDeviceReady, false);
+// // // // THESE DON'T APPEAR TO BE DOING ANYTHING
+// // // Wait for device API libraries to load
+// // function onLoad() {
+// document.addEventListener("deviceready", onDeviceReady, false);
+// // }
+//
+// // Add onResume event listener
+// function onDeviceReady() {
+//   document.addEventListener("resume", onResume, false);
+//   document.addEventListener("pause", onPause, false);
 // }
-
-// Add onResume event listener
-function onDeviceReady() {
-  document.addEventListener("resume", onResume, false);
-  document.addEventListener("pause", onPause, false);
-}
-// Handle the resume event
-function onResume() {
-  mainPageSetup();
-}
-
-function onPause() {
-  $("#logo").hide();
-  $("#button-ul").hide();
-}
+// // Handle the resume event
+// function onResume() {
+//   mainPageSetup();
+// }
+//
+// function onPause() {
+//   $("#logo").hide();
+//   $("#button-ul").hide();
+// }
 
 
 // Make website button work
@@ -59,5 +77,4 @@ $("h1").click(function() {
   $("#logo").animate({marginTop: "+=15%"}, 0);
   $("#button-ul").animate({marginTop: "-=15%"}, 0);
   mainPageSetup();
-
-  });
+});
